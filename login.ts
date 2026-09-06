6f47f3c21136a09ebc0eabef918488548f096c4c
tanmayMac <tanmayrane51@gmail.com>
2026-09-06 12:53:13 +0530

    commit 1

--- login.ts
+++ login.ts
@@ -1,3 +1,4 @@
 console.log("Login script initialized.");
 console.log("Please enter your username and password to continue.");
 console.log("added forgot password functionality");
+console.log("cherry pick commit 1");
